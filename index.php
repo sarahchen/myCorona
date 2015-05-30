a8d5f660c5e4c5abe3a421a4bdd0d17f52e8e137
Sarah Chen <sarah.jia.chen@gmail.com>
2015-05-30 16:10:24 +0000

    Add CSS

--- index.php
+++ index.php
@@ -1,8 +1,13 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title> myCorona </title>
-    </head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+        <title>myCorona</title>
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+      </head>
     <body>
         
         <! HEADING >
