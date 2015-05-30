89d9bd1b89e3dfdcaeadb22805394ceea9d5cab6
Sarah Chen <sarah.jia.chen@gmail.com>
2015-05-30 14:28:47 +0000

    Restart

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> </title>
+    </head>
+    <body>
+        
+        <div class = "head">
+            <h1>myCorona</h1>
+            
+            <div class = "nav">
+                
+            </div>
+        </div>
+        
+    </body>
+</html>
