c293cc5c8a9a4aae2fe5b941f88b0eb005a6d65a
Sarah Chen <sarah.jia.chen@gmail.com>
2015-05-30 15:12:38 +0000

    Add Navigation

---
+++ about.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> </title>
+    </head>
+    <body>
+        <h1>myCorona</h1>
+        
+        <p>myCorona is a nonprofit website service geared towards the Corona, 
+        Queens community that allows customers in the area to search and 
+        build loyalty with local businesses.</p>
+        
+    </body>
+</html>
--- index.php
+++ index.php
@@ -1,16 +1,38 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title> </title>
+        <title> myCorona </title>
     </head>
     <body>
         
+        <! HEADING >
         <div class = "head">
-            <h1>myCorona</h1>
-            
-            <div class = "nav">
-                
-            </div>
+            <nav class="navbar navbar-default">
+                <div class="container-fluid">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                    <div class="navbar-header">
+                      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                      </button>
+                      <a class="navbar-brand" href="index.php">myCorona</a>
+                    </div>
+                    
+                    <form class="navbar-form navbar-right" role="search">
+                    <div class="form-group">
+                      <input type="text" class="form-control" placeholder="Search">
+                    </div>
+                    <button type="submit" class="btn btn-default">Submit</button>
+                    </form>
+                    
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a href="about.php">About</a></li>
+                        <li><a href="login.php">Login</a></li>
+                    </ul>
+                </div><!-- /.container-fluid -->
+            </nav>
         </div>
         
     </body>
